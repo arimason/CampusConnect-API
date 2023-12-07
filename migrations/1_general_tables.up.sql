c5f4844ddd793d0fafa2ffdcf4ac8b759f132a07
junior <ari.junior@ms.touk.io>
2023-12-07 20:28:09 -0300

    att create table tb_user

--- migrations/1_general_tables.up.sql
+++ migrations/1_general_tables.up.sql
@@ -1,23 +1,10 @@
 CREATE TABLE tb_user (
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(255),
-    Email VARCHAR(255),
-    Password VARCHAR(255),
-    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DeletedAt TIMESTAMP
+    id varchar(36) not null,
+    name VARCHAR(255) not null,
+    email VARCHAR(255) not null,
+    password VARCHAR(255) not null,
+    created_at TIMESTAMP not null DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    constraint pk_tb_user primary key (id)
 );
-
--- CREATE TABLE IF NOT EXISTS tb_sys_job(
--- 	id int not null,
---   kind varchar(20) not null,
--- 	status varchar(20) not null,
--- 	processed bool not null,
---   configuration jsonb not null,
---   with_error bool not null,
--- 	result jsonb,
--- 	created_at timestamptz not null default now(),
--- 	updated_at timestamptz,
--- 	deleted_at timestamptz,
--- 	constraint pk_tb_sys_job primary key (id)
--- );
