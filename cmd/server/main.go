f96360be087e3a115d56c52a322e09384443e093
junior <ari.junior@ms.touk.io>
2023-12-07 14:26:18 -0300

    att main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,8 +18,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// criando contexto
-	ctx := context.Background()
 	// criando configuração para conexão com o postgres
 	pgConfig := &pgclient.Config{
 		Host:     cfg.DBHost,
@@ -36,8 +34,8 @@ func main() {
 	// Middleware para adicionar a conexão do banco de dados ao contexto
 	router.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// adicionando conexão do banco ao contexto
-			ctx = context.WithValue(r.Context(), "dbConn", pgConn.DBConn)
+			// criando contexto e adicionando conexão do banco ao contexto
+			ctx := context.WithValue(r.Context(), "dbConn", pgConn.DBConn)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	})
@@ -54,3 +52,37 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// func addToContext(ctx context.Context, keyValues map[string]interface{}) context.Context {
+//     for key, value := range keyValues {
+//         ctx = context.WithValue(ctx, key, value)
+//     }
+//     return ctx
+// }
+
+// // Uso da função addToContext no seu middleware
+// router.Use(func(next http.Handler) http.Handler {
+//     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//         // Extrai o token do cabeçalho da solicitação
+//         token := r.Header.Get("Authorization")
+
+//         // Cria um contexto com cancelamento e timeout, adiciona token e conexão do banco
+//         ctx := context.WithTimeout(r.Context(), 10*time.Second)
+//         ctx = addToContext(ctx, map[string]interface{}{
+//             "token":  token,
+//             "dbConn": pgConn.DBConn,
+//             // Adicione outros valores conforme necessário
+//         })
+
+//         // Defer para garantir que a conexão seja fechada no final do manipulador
+//         defer func() {
+//             if err := pgConn.DBConn.Close(); err != nil {
+//                 // Lidar com o erro de fechamento, se necessário
+//                 log.Println("Erro ao fechar a conexão com o banco de dados:", err)
+//             }
+//         }()
+
+//         // Chama o próximo manipulador com o novo contexto
+//         next.ServeHTTP(w, r.WithContext(ctx))
+//     })
+// })
