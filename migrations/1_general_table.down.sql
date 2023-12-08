a14853768a52ee11a9013e1b1434f0ba4f71791c
junior <ari.junior@ms.touk.io>
2023-12-08 10:39:01 -0300

    create new migration

--- migrations/1_general_table.down.sql
+++ migrations/1_general_table.down.sql
@@ -1,1 +1,17 @@
+-- Reverter a criação da tabela de comentários
+DROP TABLE IF EXISTS tb_comment;
+
+-- Reverter a criação da tabela de artigos
+DROP TABLE IF EXISTS tb_article;
+
+-- Reverter a criação da tabela de eventos
+DROP TABLE IF EXISTS tb_event;
+
+-- Reverter a criação da tabela de papéis
+DROP TABLE IF EXISTS tb_user_role;
+
+-- Reverter a criação da tabela de informações pessoais
+DROP TABLE IF EXISTS tb_person;
+
+-- Reverter a criação da tabela de login
 DROP TABLE IF EXISTS tb_user;
--- migrations/1_general_tables.up.sql
+++ migrations/1_general_tables.up.sql
@@ -1,10 +1,68 @@
+-- Tabela de usuário
 CREATE TABLE tb_user (
-    id varchar(36) not null,
-    name VARCHAR(255) not null,
-    email VARCHAR(255) not null,
-    password VARCHAR(255) not null,
-    created_at TIMESTAMP not null DEFAULT now(),
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
+    permission VARCHAR(20) NOT NULL, -- Pode ser 'admin', 'professor' ou 'aluno'
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    constraint pk_tb_user primary key (id)
+    CONSTRAINT pk_tb_user PRIMARY KEY (id)
 );
+
+-- Tabela de informações pessoais
+CREATE TABLE tb_person (
+    id VARCHAR(36)  NOT NULL,
+    user_id VARCHAR(36) UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_person PRIMARY KEY (id),
+    CONSTRAINT fk_tb_person_user FOREIGN KEY (user_id) REFERENCES tb_user(id)
+);
+
+-- Tabela de eventos
+CREATE TABLE tb_event (
+    id VARCHAR(36) NOT NULL,
+    creator_user_id VARCHAR(36) NOT NULL,
+    event_name VARCHAR(100),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_event PRIMARY KEY (id),
+    CONSTRAINT fk_tb_event_user FOREIGN KEY (creator_user_id) REFERENCES tb_user(id)
+);
+
+-- Tabela de artigos
+CREATE TABLE tb_article (
+    id VARCHAR(36) NOT NULL,
+    creator_user_id VARCHAR(36) NOT NULL,
+    title VARCHAR(100),
+    content TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_article PRIMARY KEY (id),
+    CONSTRAINT fk_tb_article_user FOREIGN KEY (creator_user_id) REFERENCES tb_user(id)
+);
+
+-- Tabela de comentários
+CREATE TABLE tb_comment (
+    id VARCHAR(36) NOT NULL,
+    entity_type VARCHAR(20), -- Pode ser 'article' ou 'event'
+    entity_id VARCHAR(36) NOT NULL, -- ID do artigo ou evento vinculado
+    user_id VARCHAR(36) NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_comment PRIMARY KEY (id),
+    CONSTRAINT fk_tb_comment_user FOREIGN KEY (user_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_tb_comment_article FOREIGN KEY (entity_id) REFERENCES tb_article(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tb_comment_event FOREIGN KEY (entity_id) REFERENCES tb_event(id) ON DELETE CASCADE
+);
+
+-- Outras tabelas necessárias conforme as funcionalidades específicas do seu sistema
