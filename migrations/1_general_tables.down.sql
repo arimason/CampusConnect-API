b5bd8103c5f6baf0083c0609900750bab6c54559
junior <ari.junior@ms.touk.io>
2023-12-09 09:02:36 -0300

    att migrations

--- migrations/1_general_tables.down.sql
+++ migrations/1_general_tables.down.sql
@@ -1,14 +1,35 @@
--- Reverter a criação da tabela de comentários
+-- Tabela de documentos que podem ser anexados aos eventos e artigos
+DROP TABLE IF EXISTS tb_document;
+
+-- Tabela de comentários
 DROP TABLE IF EXISTS tb_comment;
 
--- Reverter a criação da tabela de artigos
+-- Tabela de Relacionamento entre Cursos, Artigos e Eventos
+DROP TABLE IF EXISTS tb_course_content;
+
+-- Defino os valores aceitos para content_type
+DROP TYPE IF EXISTS content_type;
+
+-- Tabela de artigos
 DROP TABLE IF EXISTS tb_article;
 
--- Reverter a criação da tabela de eventos
+-- Tabela de eventos
 DROP TABLE IF EXISTS tb_event;
 
--- Reverter a criação da tabela de informações pessoais
+-- Definindo o enum para visibility
+DROP TYPE IF EXISTS visibility_type;
+
+-- Tabela de informações pessoais
 DROP TABLE IF EXISTS tb_person;
 
--- Reverter a criação da tabela de login
+-- Tabela de cursos
+DROP TABLE IF EXISTS tb_course;
+
+-- Definindo o enum para period
+DROP TYPE IF EXISTS period_type;
+
+-- Tabela de usuário
 DROP TABLE IF EXISTS tb_user;
+
+-- Definindo o enum para permission
+DROP TYPE IF EXISTS permission_type;
--- migrations/1_general_tables.up.sql
+++ migrations/1_general_tables.up.sql
@@ -1,60 +1,99 @@
+-- Definindo o enum para permission
+CREATE TYPE permission_type AS ENUM ('owner', 'admin', 'teacher','student');
+
 -- Tabela de usuário
 CREATE TABLE tb_user (
     id VARCHAR(36) NOT NULL,
     name VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    permission VARCHAR(20) NOT NULL, -- Pode ser 'admin', 'professor' ou 'aluno'
+    permission permission_type, -- Pode ser 'owner', 'admin', 'teacher','student'
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     CONSTRAINT pk_tb_user PRIMARY KEY (id)
 );
 
+-- Definindo o enum para period
+CREATE TYPE period_type AS ENUM ('morning', 'evening', 'daytime', 'night', 'all');
+
+-- Tabela de cursos
+CREATE TABLE tb_course (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    period period_type, -- Pode ser, 'morning', 'evening', 'daytime', 'night', 'all'
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_course PRIMARY KEY (id)
+);
+
 -- Tabela de informações pessoais
 CREATE TABLE tb_person (
     id VARCHAR(36)  NOT NULL,
     user_id VARCHAR(36) UNIQUE NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    course_id VARCHAR(36) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     CONSTRAINT pk_tb_person PRIMARY KEY (id),
-    CONSTRAINT fk_tb_person_user FOREIGN KEY (user_id) REFERENCES tb_user(id)
+    CONSTRAINT fk_tb_person_user FOREIGN KEY (user_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_tb_person_course FOREIGN KEY (course_id) REFERENCES tb_course(id)
 );
+
+-- Definindo o enum para period
+CREATE TYPE visibility_type AS ENUM ('pub', 'priv');
 
 -- Tabela de eventos
 CREATE TABLE tb_event (
     id VARCHAR(36) NOT NULL,
-    creator_user_id VARCHAR(36) NOT NULL,
-    event_name VARCHAR(100),
+    user_id VARCHAR(36) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    visibility visibility_type, -- Pode ser 'pub', 'priv'
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     CONSTRAINT pk_tb_event PRIMARY KEY (id),
-    CONSTRAINT fk_tb_event_user FOREIGN KEY (creator_user_id) REFERENCES tb_user(id)
+    CONSTRAINT fk_tb_event_user FOREIGN KEY (user_id) REFERENCES tb_user(id)
 );
 
 -- Tabela de artigos
 CREATE TABLE tb_article (
     id VARCHAR(36) NOT NULL,
-    creator_user_id VARCHAR(36) NOT NULL,
-    title VARCHAR(100),
-    content TEXT,
+    user_id VARCHAR(36) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     CONSTRAINT pk_tb_article PRIMARY KEY (id),
-    CONSTRAINT fk_tb_article_user FOREIGN KEY (creator_user_id) REFERENCES tb_user(id)
+    CONSTRAINT fk_tb_article_user FOREIGN KEY (user_id) REFERENCES tb_user(id)
+);
+
+-- Defino os valores aceitos para content_type
+CREATE TYPE content_type AS ENUM ('article', 'event');
+
+-- Tabela de Relacionamento entre Cursos, Artigos e Eventos
+CREATE TABLE tb_course_content (
+    course_id VARCHAR(36),
+    content_id VARCHAR(36),
+    content_type content_type, -- Pode ser 'article' ou 'event'
+    CONSTRAINT pk_tb_course_content PRIMARY KEY (course_id, content_id),
+    CONSTRAINT fk_tb_course_content_course FOREIGN KEY (course_id) REFERENCES tb_course(id),
+    CONSTRAINT fk_tb_course_content_article FOREIGN KEY (content_id) REFERENCES tb_article(id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_tb_course_content_event FOREIGN KEY (content_id) REFERENCES tb_event(id) DEFERRABLE INITIALLY DEFERRED
 );
 
 -- Tabela de comentários
 CREATE TABLE tb_comment (
     id VARCHAR(36) NOT NULL,
-    entity_type VARCHAR(20), -- Pode ser 'article' ou 'event'
+    entity_type content_type, -- Pode ser 'article' ou 'event'
     entity_id VARCHAR(36) NOT NULL, -- ID do artigo ou evento vinculado
     user_id VARCHAR(36) NOT NULL,
+    title VARCHAR(100),
     content TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP,
@@ -65,4 +104,18 @@ CREATE TABLE tb_comment (
     CONSTRAINT fk_tb_comment_event FOREIGN KEY (entity_id) REFERENCES tb_event(id) ON DELETE CASCADE
 );
 
--- Outras tabelas necessárias conforme as funcionalidades específicas do seu sistema
+-- Tabela de documentos que podem ser anexados aos eventos e artigos
+CREATE TABLE tb_document (
+    id VARCHAR(36) NOT NULL,
+    entity_type content_type, -- Pode ser 'article' ou 'event'
+    entity_id VARCHAR(36) NOT NULL, -- ID do artigo ou evento vinculado
+    user_id VARCHAR(36) NOT NULL,
+    document TEXT, -- conteúdo do documento
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT pk_tb_document PRIMARY KEY (id),
+    CONSTRAINT fk_tb_document_user FOREIGN KEY (user_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_tb_document_article FOREIGN KEY (entity_id) REFERENCES tb_article(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tb_document_event FOREIGN KEY (entity_id) REFERENCES tb_event(id) ON DELETE CASCADE
+);
