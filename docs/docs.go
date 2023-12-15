a5a08a354a3187062d46d61b84586ac0b53a8993
junior <ari.junior@ms.touk.io>
2023-12-15 16:13:44 -0300

    new swag

--- docs/docs.go
+++ docs/docs.go
@@ -238,7 +238,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "193.123.118.221:18181",
+	Host:             "134.65.242.39:18181",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Campus Connect API",
